0da3f3c73c11306042534cc640e7c2b06d49fc80
AVbeauty <123527371+AVbeauty@users.noreply.github.com>
2023-01-25 12:54:36 +0200

    Create Post.vue

---
+++ app/components/Post.vue
@@ -0,0 +1,26 @@
+<template>
+  <article>
+    <blockquote>
+      <p>{{ post }}</p>
+    </blockquote>
+  </article>
+</templates>
+
+
+<script>
+export default {
+  data: () => ({
+    post: '',
+  }),
+  async mounted() {
+    this.post = await fetch('/api/post').then((res) => res.json())
+  },
+}
+</script>
+
+<style scoped>
+blockquote {
+  border-left: 5px solid #ccc;
+  padding: 0.5rem;
+}
+</style>
